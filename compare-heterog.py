49f23ee7d0c2077d23a0bd738d40f5a7513aea2c
jFompe <jaime.fompe@gmail.com>
2023-10-26 19:17:11 +0200

    Changes for testing

--- compare-heterog.py
+++ compare-heterog.py
@@ -62,7 +62,7 @@ def run_policy(env, get_probs, get_out, nums, iters, score_type):
         for k1, js in env.JOB_SCORES().items():
             for k2, ns in env.NODE_SCORES().items():
                 results[(k1,k2)].append(sum(env.schedule_curr_sequence_reset_heterog(js, ns).values()))
-
+                print(f'{k1}{k2}: {results[(k1,k2)][-1]}')
         [o, lst] = env.combine_observations(env.build_observation(), env.build_nodes_observation())
         print ("schedule: ", end="")
         rl = 0
@@ -103,41 +103,52 @@ def run_policy(env, get_probs, get_out, nums, iters, score_type):
 
     axes.yaxis.grid(True)
     axes.set_xticks([y + 1 for y in range(len(all_data))])
-    xticklabels = [f'{k1}_{k2}' for k1 in env.JOB_SCORES() for k2 in env.NODE_SCORES()] + ['rl']
-    plt.setp(axes, xticks=[y + 1 for y in range(len(all_data))],
-             xticklabels=xticklabels)
+    xticklabels = [f'{k1}{k2}' for k1 in env.JOB_SCORES() for k2 in env.NODE_SCORES()] + ['SA']
+    plt.setp(axes, xticks=[y + 1 for y in range(len(all_data))], xticklabels=xticklabels)
     if score_type == BSLD:
-        plt.ylabel("Average bounded slowdown")
+        plt.ylabel("Average bounded slowdown", fontsize=15)
     elif score_type == AVGW:
-        plt.ylabel("Average waiting time")
+        plt.ylabel("Average waiting time", fontsize=15)
     elif score_type == AVGT:
-        plt.ylabel("Average turnaround time")
+        plt.ylabel("Average turnaround time", fontsize=15)
     elif score_type == RESU:
-        plt.ylabel("Resource utilization")
+        plt.ylabel("Resource utilization", fontsize=15)
     elif score_type == SLD:
-        plt.ylabel("Slowdown")
+        plt.ylabel("Slowdown", fontsize=15)
     else:
         raise NotImplementedError
 
+    # plt.title('Comparison of results for the Squared Agent with Clustering (BSLD).', fontsize=14)
+    # plt.title('Comparison of results for the Squared Agent (BSLD). Train: lublin_256 | Test: RICC-2010-2', fontsize=18)
+    plt.title('Comparison of results for the Squared Agent (AVGW)', fontsize=18)
+    # plt.title('Comparison of results for the Squared Agent with clustering (BSLD)', fontsize=18)
     # plt.ylabel("Average waiting time (s)")
-    plt.xlabel("Scheduling Policies")
+    plt.xlabel("Scheduling Policies", fontsize=15)
     # plt.tick_params(axis='both', which='major', labelsize=40)
     # plt.tick_params(axis='both', which='minor', labelsize=40)
-    plt.tick_params(axis='both', which='major', labelsize=20)
-    plt.tick_params(axis='both', which='minor', labelsize=20)
+    plt.tick_params(axis='both', which='major', labelsize=15)
+    plt.tick_params(axis='both', which='minor', labelsize=15)
 
-    plt.savefig('data/graphics/fig_1net_BSLD_x200_clustering.png')
+    # plt.savefig('data/NuevosExperimentos/DiferenteObjectivo/Figures/BoxplotExp_SLD_BSLD.png')
+    plt.savefig('data/Pruebas/Test1epoch.png')
+    # plt.savefig('data/ExperimentosTFG/Figures/BoxplotExp5_SqSLD.png')
+    # plt.savefig('data/ExperimentosTFG/Figures/BoxplotExp6_SqAVGW.png')
+    # plt.savefig('data/ExperimentosTFG/Figures/BoxplotExp7_SqBSLD_Clust_NEW.png')
 
 if __name__ == '__main__':
     import argparse
     import time
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--rlmodel', type=str, default="./data/logs/1net_BSLD_x200_clustering/1net_BSLD_x200_clustering_s1804")
+    # parser.add_argument('--rlmodel', type=str, default="./data/logs/Exp4_SquaredSLD/Exp4_SquaredSLD_s2406")
+    # parser.add_argument('--rlmodel', type=str, default="./data/logs/Exp5_SquaredSLD/Exp5_SquaredSLD_s2406")
+    parser.add_argument('--rlmodel', type=str, default="./data/logs/Exp6_SquaredAVGW/Exp6_SquaredAVGW_s2406")
+    # parser.add_argument('--rlmodel', type=str, default="./data/logs/Exp6_SquaredAVGW/Exp6_SquaredAVGW_s2406")
+    # parser.add_argument('--rlmodel', type=str, default="./data/logs/Exp7_SquaredClustering/Exp7_SquaredClustering_s2406")
     parser.add_argument('--workload', type=str, default='./data/lublin_256.swf')
     parser.add_argument('--platform', type=str, default='./data/cluster_x4_64procs.json')
-    parser.add_argument('--len', '-l', type=int, default=2049)
-    parser.add_argument('--seed', '-s', type=int, default=1234)
+    parser.add_argument('--len', '-l', type=int, default=1024)
+    parser.add_argument('--seed', '-s', type=int, default=500)
     parser.add_argument('--iter', '-i', type=int, default=20)
     parser.add_argument('--shuffle', type=int, default=0)
     parser.add_argument('--backfil', type=int, default=0)
@@ -145,6 +156,7 @@ if __name__ == '__main__':
     parser.add_argument('--score_type', type=int, default=0)
     parser.add_argument('--batch_job_slice', type=int, default=0)
     parser.add_argument('--enable_clustering', type=int, default=0)
+    # parser.add_argument('--enable_clustering', type=int, default=1)
 
     args = parser.parse_args()
 
@@ -160,6 +172,7 @@ if __name__ == '__main__':
                  batch_job_slice=args.batch_job_slice, build_sjf=False, enable_clustering=args.enable_clustering)
     env.my_init(workload_file=workload_file, platform_file=platform_file)
     env.seed(args.seed)
+    random.seed(args.seed)
 
     start = time.time()
     run_policy(env, get_probs, get_value, args.len, args.iter, args.score_type)
