4b187fc3c764ff490572b689f3eb62781df3df88
Esteban Stafford <esteban.stafford@unican.es>
2023-11-20 12:32:37 +0100

    Set seed as variable

--- run_experiment.sh
+++ run_experiment.sh
@@ -3,6 +3,7 @@
 PYTHON=python3
 WORKLOAD="data/lublin_256.swf"
 PLATFORM="data/cluster_x4_64procs.json"
+TRAIN_SEED=2406
 
 scores=(BSLD AVGW AVGT RESU SLD)
 
@@ -17,7 +18,7 @@ for score in 0 1 4; do
      --workload $WORKLOAD \
      --platform $PLATFORM \
      --gamma 0.99 \
-     --seed 2406 \
+     --seed $TRAIN_SEED \
      --trajs 20 \
      --epochs 100 \
      --exp_name model_${scores[$score]} \
