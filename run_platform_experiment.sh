84c1b299806545dcda710b18afaa1890e02c3307
Esteban Stafford <esteban.stafford@unican.es>
2023-11-22 18:38:13 +0100

    Create a set of platforms with varying heterogeneity

---
+++ run_platform_experiment.sh
@@ -0,0 +1,4 @@
+data/generate_platform 6,64,4.2 > data/homo.json
+data/generate_platform 2,64,{2.1,3,3.5,4} > data/hetero_freq.json
+data/generate_platform 3,{4,8,16,32,64},4.335 > data/hetero_core.json
+data/generate_platform 1,{4,8,16,32,64},{2.5,3,3.5,4} > data/hetero.json
