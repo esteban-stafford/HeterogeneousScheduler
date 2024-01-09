0ff3b37fd5d7a5bcfe25feb9cc3bc650a0d20071
Esteban Stafford <esteban.stafford@unican.es>
2024-01-09 16:46:33 +0100

    Moved to research repo

--- copy_progress.sh
+++
@@ -1,8 +0,0 @@
-
-for model in model:{lublin_256,lublin_1024,PIK-IPLEX-2009-1,CTC-SP2-1996-3.1-cln}:{homo,hetero}:BSLD; do 
-   cp data/logs/$model/${model}_s2406/progress.txt ../${model}_s2406_progress.txt
-done
-for model in model:cl{4,8,16}:KIT-FH2-2016-1:{homo,hetero}_x8:BSLD; do 
-   cp data/logs/$model/${model}_s2406/progress.txt ../${model}_s2406_progress.txt
-done
-cp data/logs/compare_models:*{lub,PIK,CTC,KIT}*:{hetero,homo}*.dat ..
